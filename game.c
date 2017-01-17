76e0dfe1396ba5498f4f531dc1181dab2bfa3634
IAsWell <jyang1737@gmail.com>
2017-01-17 14:56:17 -0500

    SAf

---
+++ game.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parse.h"
+#include "piece.h"
+
+int main() {
+  
+  Piece ** pieces; 
+
+  add(pieces);
+
+
+
+}
--- piece.c
+++ piece.c
@@ -5,11 +5,29 @@ typedef struct {
 
   char * name;
   char coord[2];
-
+  int color; //0: white, 1: black
+  
 } Piece;
 
-char move(Piece piece) {
+char * move(Piece piece, char dest[]) {
   
+  char curCoord[2];
+  curCoord = piece.coord;
+  piece.coord = dest;
+  return curCoord;
+
+}
+
+char * check(Piece piece, char dest[]) {
   
+  char * name = piece.name;
   
+  //  if (strcmp(name, "pn"))
+    //    if (
+}
+
+void add(pieces) {
+
+  
+
 }
