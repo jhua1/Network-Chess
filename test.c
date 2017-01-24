f4d93c8b89cc3889b4923ace5f92178256f84a58
IAsWell <jyang1737@gmail.com>
2017-01-24 01:21:20 -0500

    f

--- test.c
+++ test.c
@@ -83,7 +83,7 @@ int * translatePiece(char * piece) {
 
 }
 
-int checkMoves(int * piece, int * from, int * to) {
+int checkMoves(int * piece, int * from, int * to, piece board[8][8]) {
   
   int color = piece[0];
   int type = piece[1];
@@ -91,48 +91,792 @@ int checkMoves(int * piece, int * from, int * to) {
   int y1 = from[1];
   int x2 = to[0];
   int y2 = to[1];
-
-  if (x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7 || x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0)
+  
+  if ((x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7 || x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0) && (x1 != x2 && y1 != y2))
     return 0;
-
+  
   //pawn
-  if (type == 0) {1
-    if ( (x2-x1) == 1 && (y2-y1) == 1) {
-      if (
+  if (type == 0) {
+    //black
+    if (color == 0) { 
+      //starting position
+      if (y1 == 6) {
+	if ((y2-y1) == -2 || (y2-y1) == -1) {
+	  //straight forward
+	  if (x2-x1 == 0) {
+	    //nothing is in to
+	    if (strcmp(board[x2][y2].name, "---") == 0)
+	      return 1;
+	    //something is there
+	    else
+	      return 0;
+	  }
+	  //diagonal
+	  else if (abs(x2-x1) == 1) {
+	    //white piece
+	    if (board[x2][y2].color == 1)
+	      return 0;
+	    //either nothing, or own piece
+	    else
+	      return 0;
+	  }
+	  else 
+	    return 0;
+	}
+	//moving too many spaces forward/backward
+	else 
+	  return 0;
+      }
+      //anywhere else
+      else {
+	//moves 1 unit
+	if ((y2-y1) == -1) {
+	  //straight forward
+	  if (x2-x1 == 0) {
+	    //nothing is in to
+	    if (strcmp(board[x2][y2].name, "---") == 0)
+	      return 1;
+	    //something is there
+	    else
+	      return 0;
+	  }
+	  //diagonal
+	  else if (abs(x2-x1) == 1) {
+	    //white piece
+	    if (board[x2][y2].color == 1)
+	      return 1;
+	    //either nothing, or own piece
+	    else
+	      return 0;
+	  }
+	}
+      }
+    }
+    //white
+    if (color == 1) {
+      //starting position
+      if (y1 == 1) {
+	if ((y2-y1) == 2 || (y2-y1) == 1) {
+	  //straight forward
+	  if (x2-x1 == 0) {
+	    //nothing is in to
+	    if (strcmp(board[x2][y2].name, "---") == 0)
+	      return 1;
+	    //something is there
+	    else
+	      return 0;
+	  }
+	  //diagonal
+	  else if (abs(x2-x1) == 1) {
+	    //black piece
+	    if (board[x2][y2].color == 0)
+	      return 1;
+	    //either nothing, or own piece
+	    else
+	      return 0;
+	  }
+	  else 
+	    return 0;
+	}
+	//moving too many spaces forward/backward
+	else 
+	  return 0;
+      }
+      //anywhere else
+      else {
+	//moves 1 unit
+	if ((y2-y1) == 1) {
+	  //straight forward
+	  if (x2-x1 == 0) {
+	    //nothing is in to
+	    if (strcmp(board[x2][y2].name, "---") == 0)
+	      return 1;
+	    //something is there
+	    else
+	      return 0;
+	  }
+	  //diagonal
+	  else if (abs(x2-x1) == 1) {
+	    //black piece
+	    if (board[x2][y2].color == 0)
+	      return 0;
+	    //either nothing, or own piece
+	    else
+	      return 0;
+	  }
+	}
+      }
+    }
+  }
+  
+  //bishop
+  if (type == 1) {
+    //black
+    if (color == 0) {
+      //moving diagonally
+      if (abs(y2-y1) == abs(x2-x1)) {
+	if (x2 > x1 && y2 > y1) {
+	  //increment x, increment y
+	  int tempx = x1;
+	  int tempy = y1;
+	  while(tempx != x2 || tempy != y2) {
+	    tempx++;
+	    tempy++;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      
+	if (x2 > x1 && y2 < y1) {
+	  //increment x, decrement y
+	  int tempx = x1;
+	  int tempy = y1;
+	  while (tempx != x2 || tempy != y1) {
+	    tempx++;
+	    tempy--;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+	if (x2 < x1 && y2 > y1) {
+	  //decrement x, increment y
+	  int tempx = x2;
+	  int tempy = y1;
+	while (tempx != x1 || tempy != y2) {
+	  tempx--;
+	  tempy++;
+	  if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	    return 0;
+	}
+	if (strcmp(board[tempx][tempy].name, "---") == 0)
+	  return 1;
+	else if (board[tempx][tempy].color == 1)
+	  return 1;
+	else 
+	  return 0;
+	}
+	if (x2 < x1 && y2 < y1) {
+	  //decrement x, decrement y
+	  int tempx = x2;
+	  int tempy = y2;
+	  while (tempx != x1 || tempy != y1) {
+	    tempx--;
+	    tempy--;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;	 
+	}
+      }
+    }
+  }
+  //white
+  if (color == 1) { 
+    //moving diagonally
+    if (abs(y2-y1) == abs(x2-x1)) {
+      if (x2 > x1 && y2 > y1) {
+	//increment x, increment y
+	int tempx = x1;
+	int tempy = y1;
+	while(tempx != x2 || tempy != y2) {
+	  tempx++;
+	  tempy++;
+	  if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	    return 0;
+	}
+	if (strcmp(board[tempx][tempy].name, "---") == 0)
+	  return 1;
+	else if (board[tempx][tempy].color == 0)
+	  return 1;
+	else 
+	  return 0;
+      }
+    }
+    if (x2 > x1 && y2 < y1) {
+      //increment x, decrement y
+      int tempx = x1;
+      int tempy = y1;
+      while (tempx != x2 || tempy != y1) {
+	tempx++;
+	tempy--;
+	if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	  return 0;
+      }
+      if (strcmp(board[tempx][tempy].name, "---") == 0)
+	return 1;
+      else if (board[tempx][tempy].color == 0)
+	return 1;
+      else 
+	return 0;
+    }
+    if (x2 < x1 && y2 > y1) {
+      //decrement x, increment y
+      int tempx = x2;
+      int tempy = y1;
+      while (tempx != x1 || tempy != y2) {
+	tempx--;
+	tempy++;
+	if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	  return 0;
+      }
+      if (strcmp(board[tempx][tempy].name, "---") == 0)
+	return 1;
+      else if (board[tempx][tempy].color == 0)
+	return 1;
+      else 
+	return 0;
+    }
+    if (x2 < x1 && y2 < y1) {
+      //decrement x, decrement y
+      int tempx = x2;
+      int tempy = y2;
+      while (tempx != x1 || tempy != y1) {
+	tempx--;
+	tempy--;
+	if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	  return 0;
+      }
+      if (strcmp(board[tempx][tempy].name, "---") == 0)
+	return 1;
+      else if (board[tempx][tempy].color == 0)
+	return 1;
+      else 
+	return 0;	 
     }
   }
 
-  //bishop
-  if (type == 1) {
-
-  }
   
   //knight
   if (type == 2) {
-
+    if (color == 0) {
+      if (abs(x2-x1) == 1 && abs(y2-y1) == 2) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 1)
+	  return 1;
+	else 
+	  return 0;
+      }
+      if (abs(x2-x1) == 2 && abs(y2-y1) == 1) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 1)
+	  return 1;
+	else
+	  return 0;
+      }
+    }
+    if (color == 1) {
+      if (abs(x2-x1) == 1 && abs(y2-y1) == 2) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 0)
+	  return 1;
+	else 
+	  return 0;
+      }
+      if (abs(x2-x1) == 2 && abs(y2-y1) == 1) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 0)
+	  return 1;
+	else
+	  return 0;
+      }
+    }
   }
 
   //rook
   if (type == 3) {
-
+    if (color == 0) {
+      if (x2-x1 == 0) {
+	if (y2 > y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy++;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 1)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (y2 < y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy--;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+      if (y2-y1 == 0) {
+	if (x2 > x1) {
+	  int tempx = x1;
+	  while (tempx != x2) {
+	    tempx++;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 1)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (x2 < x1) {
+	  int tempx= x1;
+	  while (tempx != x2) {
+	    tempx--;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+       
+    }
+    if (color == 1) {
+      if (x2-x1 == 0) {
+	if (y2 > y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy++;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 0)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (y2 < y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy--;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 0)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+      if (y2-y1 == 0) {
+	if (x2 > x1) {
+	  int tempx = x1;
+	  while (tempx != x2) {
+	    tempx++;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 0)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (x2 < x1) {
+	  int tempx= x1;
+	  while (tempx != x2) {
+	    tempx--;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 0)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+       
+    }
   }
 
   //queen
   if (type == 4) {
-
+    if (color == 0) {
+      //diagonals
+      if (abs(y2-y1) == abs(x2-x1)) {
+	if (x2 > x1 && y2 > y1) {
+	  //increment x, increment y
+	  int tempx = x1;
+	  int tempy = y1;
+	  while(tempx != x2 || tempy != y2) {
+	    tempx++;
+	    tempy++;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+	
+	if (x2 > x1 && y2 < y1) {
+	  //increment x, decrement y
+	  int tempx = x1;
+	  int tempy = y1;
+	  while (tempx != x2 || tempy != y1) {
+	    tempx++;
+	    tempy--;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+	if (x2 < x1 && y2 > y1) {
+	  //decrement x, increment y
+	  int tempx = x2;
+	  int tempy = y1;
+	  while (tempx != x1 || tempy != y2) {
+	    tempx--;
+	    tempy++;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+	if (x2 < x1 && y2 < y1) {
+	  //decrement x, decrement y
+	  int tempx = x2;
+	  int tempy = y2;
+	  while (tempx != x1 || tempy != y1) {
+	    tempx--;
+	    tempy--;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;	 
+	}
+      }
+      //horizontal
+      if (x2-x1 == 0) {
+	if (y2 > y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy++;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 1)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (y2 < y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy--;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+      //vertical
+      if (y2-y1 == 0) {
+	if (x2 > x1) {
+	  int tempx = x1;
+	  while (tempx != x2) {
+	    tempx++;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 1)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (x2 < x1) {
+	  int tempx= x1;
+	  while (tempx != x2) {
+	    tempx--;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 1)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+       
+    }
+    if (color == 1) {
+      //diagonals
+      if (abs(y2-y1) == abs(x2-x1)) {
+	if (x2 > x1 && y2 > y1) {
+	  //increment x, increment y
+	  int tempx = x1;
+	  int tempy = y1;
+	  while(tempx != x2 || tempy != y2) {
+	    tempx++;
+	    tempy++;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 0)
+	    return 1;
+	  else 
+	    return 0;
+	}
+	
+	if (x2 > x1 && y2 < y1) {
+	  //increment x, decrement y
+	  int tempx = x1;
+	  int tempy = y1;
+	  while (tempx != x2 || tempy != y1) {
+	    tempx++;
+	    tempy--;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 0)
+	    return 1;
+	  else 
+	    return 0;
+	}
+	if (x2 < x1 && y2 > y1) {
+	  //decrement x, increment y
+	  int tempx = x2;
+	  int tempy = y1;
+	  while (tempx != x1 || tempy != y2) {
+	    tempx--;
+	    tempy++;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 0)
+	    return 1;
+	  else 
+	    return 0;
+	}
+	if (x2 < x1 && y2 < y1) {
+	  //decrement x, decrement y
+	  int tempx = x2;
+	  int tempy = y2;
+	  while (tempx != x1 || tempy != y1) {
+	    tempx--;
+	    tempy--;
+	    if (!(strcmp(board[tempx][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][tempy].color == 0)
+	    return 1;
+	  else 
+	    return 0;	 
+	}
+      }
+      //horizontal
+      if (x2-x1 == 0) {
+	if (y2 > y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy++;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 0)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (y2 < y1) {
+	  int tempy = y1;
+	  while (tempy != y2) {
+	    tempy--;
+	    if (!(strcmp(board[x2][tempy].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[x2][tempy].name, "---") == 0)
+	    return 1;
+	  else if (board[x2][tempy].color == 0)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+      //vertical
+      if (y2-y1 == 0) {
+	if (x2 > x1) {
+	  int tempx = x1;
+	  while (tempx != x2) {
+	    tempx++;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 0)
+	    return 1;
+	  else
+	    return 0;
+	}
+	if (x2 < x1) {
+	  int tempx= x1;
+	  while (tempx != x2) {
+	    tempx--;
+	    if (!(strcmp(board[tempx][y2].name, "---") == 0))
+	      return 0;
+	  }
+	  if (strcmp(board[tempx][y2].name, "---") == 0)
+	    return 1;
+	  else if (board[tempx][y2].color == 0)
+	    return 1;
+	  else 
+	    return 0;
+	}
+      }
+      
+ }
   }
 
   //king
   if (type == 5) {
-
+    //black
+    if (color == 0) {
+      //vertical
+      if (abs(x2-x1) == 1 && (y2-y1) == 0) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 1)
+	  return 1;
+	else
+	  return 0;
+      }
+      //horizontal 
+      if ((x2-x1) == 0 && abs(y2-y1) == 1) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 1)
+	  return 1;
+	else
+	  return 0;
+      }
+      //diagonal
+      if (abs(x2-x1) == 1 && abs(y2-y1) == 1) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;	
+	else if (board[x2][y2].color == 1)
+	  return 1;
+	else return 0;
+      }
+    }
+    //white
+    if (color == 1) {
+      //vertical
+      if (abs(x2-x1) == 1 && (y2-y1) == 0) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 0)
+	  return 1;
+	else
+	  return 0;
+      }
+      //horizontal 
+      if ((x2-x1) == 0 && abs(y2-y1) == 1) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;
+	else if (board[x2][y2].color == 0)
+	  return 1;
+	else
+	  return 0;
+      }
+      //diagonal
+      if (abs(x2-x1) == 1 && abs(y2-y1) == 1) {
+	if (strcmp(board[x2][y2].name, "---") == 0)
+	  return 1;	
+	else if (board[x2][y2].color == 0)
+	  return 1;
+	else return 0;
+      }
+    }
   }
-
-
   return 0;
 }
 
-int check(char ** input) {
-
+void move(char ** input, piece board[8][8]) {
+  
   int *piece;
   int *from;
   int *to;
@@ -141,30 +885,65 @@ int check(char ** input) {
   from = translateCoord(input[1]);
   to = translateCoord(input[2]);
 
+  int color = piece[0];
+  int type = piece[1];
+  int x1 = from[0]; 
+  int y1 = from[1];
+  int x2 = to[0];
+  int y2 = to[1];
+  
+  char * tempName = (char *) malloc(sizeof(char) *3);
+  int tempColor;
+
   if (checkMoves(piece, from, to)) {
+    
+    tempName = board[x1][y1].name;
+    tempColor = board[x1][y1].color;
+    
+    board[x2][y2].name = board[x1][y1].name;
+    board[x2][y2].color = board[x1][y1].color;
+    
+    board[x1][y1].name = "---";
+    
+    
+
     free(piece);
     free(from);
     free(to);
     return 1;
   }
+
   else {
+    printf("Invalid move");
     free(piece);
     free(from);
     free(to);
     return 0;
   }
+      
+    
+    
+  }
+   
 }
-
+  
 int main(int argc, char argv[]) {
-  char buffer[1000];
+  
+  int x = -1;
+  printf("%d\n",x );
 
+  /*
+  char buffer[1000];
+  
   printf("type");
   fgets(buffer, sizeof(buffer), stdin);
   char ** inputs = parse(buffer);
   check(inputs);
   return 0;
 
-  /*
+  
+
+  
   char * input = "f4";
   int * res;
   //  printf("%s\n", input);
